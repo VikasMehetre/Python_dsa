804ae746a0d7e184ff7db827c41837affd445eee
vikas mehetre <vikasmehetre0910@gmail.com>
2023-02-15 19:27:09 +0530

    First Vommit

---
+++ ord.py
@@ -0,0 +1,11 @@
+x="xaa"
+y="axx"
+print(x>y)
+print(ord("x"))
+print(ord("a"))# returns the unicode value
+x="gcgcgrue"
+y="true"
+print(x.lower ==y)
+print(x or y)
+num1=input()
+print(type(num1))
---
+++ strings_1.py
@@ -0,0 +1,13 @@
+from math import ceil
+
+
+str1='''Hello "world" people's'''
+print(str1)
+#nine type
+x=10
+y=print(x)
+print(y)
+c=4.5*2
+print(type(c))
+print(c)
+print(ceil(45/4))
